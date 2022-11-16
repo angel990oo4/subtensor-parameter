39bbd8144842f8daaeca654cc19bab471a3225d8
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-11-16 13:25:15 -0500

    clean code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import bittensor
 from flask import Flask, jsonify, request
 import json
 from flask_cors import CORS
-from tqdm import tqdm
+# from tqdm import tqdm
 # from concurrent.futures import ThreadPoolExecutor, as_completed
 import rich, subtensorapi
 app = Flask(__name__)
@@ -12,9 +12,21 @@ sub = bittensor.subtensor()
 @app.route('/')
 def hello_world():
     return 'Bittensor Cli Bot'
+
 @app.route('/bot')
 def bit_bot():
     return jsonify({"block": f"{str(sub.block)}", "difficulty": f"{str(sub.difficulty)}", "totalIssuance": f"{str(sub.total_issuance)}" })
+
+@app.route("/subtensorapi")
+def bit_subtensorapi():
+    print('request.get_json()', request.get_json())
+    uid = request.get_json()['uid']
+    days = int(request.get_json()['range'])
+    blocks = list(range(sub.block - (7200 * days), sub.block, 7200))
+    fastsync = subtensorapi.FastSync("wss://archivelb.nakamoto.opentensor.ai:9943")
+    fastsync.sync_and_save_historical(rich.console.Console(), blocks, [uid], "FILENAMETHATYOUCANACCESS")
+    historical_data = fastsync.load_historical_neurons("FILENAMETHATYOUCANACCESS")
+    return json.dumps(historical_data, default=lambda x: x.__dict__)
 
 # @app.route("/stake")
 # def bit_stake():
@@ -51,21 +63,11 @@ def bit_bot():
 #                     pbar.update(1)
 #     return jsonify({"stake_range":stake_range})
 
-@app.route("/subtensorapitest")
-def bit_subtensorapi_test():
-    print('request', request.get_json()['uid'])
+# @app.route("/subtensorapitest")
+# def bit_subtensorapi_test():
+#     print('request', request.get_json()['uid'])
 
-    return "Test history data"
+#     return "Test history data"
 
-@app.route("/subtensorapi")
-def bit_subtensorapi():
-    print('request.get_json()', request.get_json())
-    uid = request.get_json()['uid']
-    days = int(request.get_json()['range'])
-    blocks = list(range(sub.block - (7200 * days), sub.block, 7200))
-    fastsync = subtensorapi.FastSync("wss://archivelb.nakamoto.opentensor.ai:9943")
-    fastsync.sync_and_save_historical(rich.console.Console(), blocks, [uid], "FILENAMETHATYOUCANACCESS")
-    historical_data = fastsync.load_historical_neurons("FILENAMETHATYOUCANACCESS")
-    return json.dumps(historical_data, default=lambda x: x.__dict__)
 if __name__ == '__main__':
    app.run(debug=True)
