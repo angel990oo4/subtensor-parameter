dcaa29b48e851af0b08bc353ce7ad02de47f3884
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-12-08 16:50:49 -0500

    fixed fastsync runtime error

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import rich, subtensorapi
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 sub = bittensor.subtensor()
+fastsync = subtensorapi.FastSync("wss://archivelb.nakamoto.opentensor.ai:9943")
 
 @app.route('/')
 def hello_world():
@@ -22,7 +23,6 @@ def bit_subtensorapi():
     uid = request.get_json()['uid']
     days = int(request.get_json()['range'])
     blocks = list(range(sub.block - (7200 * days), sub.block, 7200))
-    fastsync = subtensorapi.FastSync("wss://archivelb.nakamoto.opentensor.ai:9943")
     fastsync.sync_and_save_historical(rich.console.Console(), blocks, [uid], "FILENAMETHATYOUCANACCESS")
     historical_data = fastsync.load_historical_neurons("FILENAMETHATYOUCANACCESS")
     return json.dumps(historical_data, default=lambda x: x.__dict__)
